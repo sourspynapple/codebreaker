f33502a66abd3fcc6cdb872a369b2e823406c53c
sourspynapple <labi.eleanor@emanuelschool.nsw.edu.au>
2017-07-02 13:54:38 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function init() {
  alert("Page is loaded");
- codeGenerator()
+ //codeGenerator()
 }
 
-function codeGenerator() {
-code = Math.floor(
-Math.rand() * 10000 + 1);
-document.getElementById("answer").innerHTML = code;
+//function codeGenerator() {
+//code = Math.floor(
+//Math.rand() * 10000 + 1);
+//document.getElementById("answer").innerHTML = code;
 
-}
+//}
