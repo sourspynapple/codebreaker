9a247e88f75de6b8fcef8202713f622f1c321544
sourspynapple <labi.eleanor@emanuelschool.nsw.edu.au>
2017-07-02 13:44:56 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
+function init() {
+ codeGenerator()
+}
+
 function codeGenerator() {
 code = Math.floor(
 Math.rand() * 10000 + 1);
-document.getElementById("answer").innerHTML = 
+document.getElementById("answer").innerHTML = code;
 
-
-
-<p id="random"></p>
-<p id="demo"></p>
-var rand_num = Math.random();
-
-document.getElementById("random").innerHTML = rand_num;
-
-document.getElementById("demo").innerHTML =
-Math.floor(rand_num  * 10000) + 1;
+}
